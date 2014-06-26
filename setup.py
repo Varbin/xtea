ee1955b8c698a7aa03eb9b232e82e8bc5b4d9ed2
Varbin <simon.biewald@web.de>
2014-06-26 17:04:54 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+with open("README") as rm:
+  long_text = rm.read()
+
+setup(name='xtea',
+      version='0.1',
+      description="A python version of XTEA",
+      long_description = long_text,
+      author="Simon Biewald",
+      py_modules=['xtea'],
+      classifiers=[
+	"Development Status :: 4 - Beta",
+	"Operating System :: OS Independent",
+	"Programming Language :: Python :: 2 :: Only",
+	"Topic :: Security :: Cryptography"]
+      )
