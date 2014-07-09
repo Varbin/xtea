cd73a6487fc319342118cccc8cea639740475b8c
Varbin <simon.biewald@web.de>
2014-07-09 18:15:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 with open("README.rst") as rm:
-  long_text = rm.read()
+    long_text = rm.read()
 
 setup(name='xtea',
-      version='0.1',
+      version='0.1.1',
       description="A python version of XTEA",
       long_description = long_text,
       author="Simon Biewald",
+      author_email="simon.biewald@hotmail.de",
       py_modules=['xtea'],
       classifiers=[
 	"Development Status :: 4 - Beta",
