74acee782177c3a50ada42c38f8b9ec09e60a51f
Varbin <simon.biewald@hotmail.de>
2016-10-16 11:21:42 +0200

    Updated setup.py, version number, removed PY-3 warning

--- setup.py
+++ setup.py
@@ -2,9 +2,6 @@ from __future__ import with_statement  # Reading README
 import sys
 import warnings
 
-if sys.hexversion >=0x03000000:
-    warnings.warn( # Py3 for uploading
-        "This module will not work with python 3+")
 
 try:
     from setuptools import setup
@@ -21,7 +18,7 @@ def get_file(name):
 long_text = get_file("README.rst") + "\n\n" + get_file("changelog.rst")
 
 setup(name='xtea',
-      version='0.4.1',
+      version='0.6.0',
       description="A python version of XTEA",
       long_description = long_text,
       author="Simon Biewald",
@@ -36,7 +33,8 @@ setup(name='xtea',
 	"Development Status :: 4 - Beta",
         "License :: Public Domain",
 	"Operating System :: OS Independent",
-	"Programming Language :: Python :: 2 :: Only",
+	"Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: IronPython",
         "Programming Language :: Python :: Implementation :: Jython",
