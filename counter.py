3622b05a880938137e344168b7d96c95e9c3c979
HolyGuacamole <HolyGuacamole@users.noreply.github.com>
2017-01-15 23:11:23 +0100

    implemented tests for bytes to int conversion

--- counter.py
+++ counter.py
@@ -1,5 +1,34 @@
-################ Basic counter for CTR mmode
+################ Basic counter for CTR mode
 import array
+import sys
+import struct
+
+PY_3 = sys.version_info.major >= 3
+
+if PY_3:
+    def to_bytes(integer, length, byteorder):
+        return integer.to_bytes(length, byteorder)
+
+    def from_bytes(bytesarray, byteorder):
+        return int.from_bytes(bytesarray, byteorder)
+else:
+    def to_bytes(integer, length, byteorder='big'):
+        h = '%x' % integer    
+        s = ('0'*(len(h) % 2) + h).zfill(length*2).decode('hex')
+        if byteorder == 'big':
+            return s 
+        elif byteorder =='little':
+            return s[::-1]
+        else:
+            raise ValueError("byteorder must be either 'little' or 'big'")
+
+    def from_bytes(bytesarray, byteorder):
+        if byteorder=='big':
+            return struct.unpack(">L", bytesarray)[0]
+        elif byteorder=='little':
+            return struct.unpack("<L", bytesarray)[0]
+        else:
+            raise ValueError("byteorder must be either 'little' or 'big'")
 
 class Counter:
     """Small counter for CTR mode, based on arrays
@@ -19,7 +48,7 @@ class Counter:
         b'%2dUI84e'
     """
     
-    def __init__(self, nonce):
+    def __init__(self, nonce, byteorder='big'):
         """Constructor for a counter which is suitable for CTR mode.
         Args:
             nonce (bytes): The start value, \
@@ -27,6 +56,7 @@ class Counter:
         """
 
         self.__nonce = nonce
+        self.byteorder = byteorder
         self.reset()
 
     def __call__(self):
---
+++ testCounter.py
@@ -0,0 +1,44 @@
+import unittest
+
+from counter import Counter, to_bytes, from_bytes
+
+class TestIntToBytes(unittest.TestCase):
+
+    def testLittleEndian(self):
+        result = to_bytes(67305985, 4, 'little')
+        expected = b'\x01\x02\x03\x04'
+        self.assertEqual(result, expected)
+            
+    def testBigEndian(self):
+        result = to_bytes(16909060, 4, 'big')
+        expected = b'\x01\x02\x03\x04'
+        self.assertEqual(result, expected)
+    
+    def testStrangeByteOrder(self):
+        with self.assertRaises(ValueError):
+            to_bytes(100, 4, 'banana')
+
+class TestBytesToInt(unittest.TestCase):
+
+    def testLittleEndian(self):
+        result = from_bytes(b'\x01\x02\x03\x04', 'little')
+        expected = 67305985
+        self.assertEqual(result, expected)
+    
+    def testBigEndian(self):
+        result = from_bytes(b'\x01\x02\x03\x04', 'big')
+        expected = 16909060
+        self.assertEqual(result, expected)
+    
+    def testStrangeByteOrder(self):
+        with self.assertRaises(ValueError):
+            from_bytes(b'\x01\x02\x03\x04', 'banana')
+    
+class TestCounter(unittest.TestCase):
+    
+    def testCountUp(self):
+        pass
+    
+    def testCountDown(self):
+        pass
+
