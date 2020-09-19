cc4e0a54943103ff33cc0e85d2704aa712e325c5
Simon Biewald <simon@fam-biewald.de>
2020-09-19 12:17:25 +0200

    Rename to snake_case, add or ignore missing docstring

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -1,52 +1,70 @@
+"""
+Test the counter component.
+"""
+
 import unittest
 
 from xtea.counter import Counter, to_bytes, from_bytes
 
+# pylint: disable=missing-function-docstring
+
 
 class TestIntToBytes(unittest.TestCase):
-    def testLittleEndian(self):
+    """
+    Test the conversion of integers to bytes.
+    """
+
+    def test_little_endian(self):
         result = to_bytes(0x04030201, 4, 'little')
         expected = b'\x01\x02\x03\x04'
         self.assertEqual(result, expected)
 
-    def testBigEndian(self):
+    def test_big_endian(self):
         result = to_bytes(0x01020304, 4, 'big')
         expected = b'\x01\x02\x03\x04'
         self.assertEqual(result, expected)
 
-    def testStrangeByteOrder(self):
+    def test_invalid_byte_order(self):
         with self.assertRaises(ValueError):
             to_bytes(100, 4, 'banana')
 
 
 class TestBytesToInt(unittest.TestCase):
-    def testLittleEndian(self):
+    """
+    Test the conversion of bytes to integers.
+    """
+
+    def test_little_endian(self):
         result = from_bytes(b'\x01\x02\x03\x04', 'little')
         expected = 0x04030201
         self.assertEqual(result, expected)
 
-    def testBigEndian(self):
+    def test_big_endian(self):
         result = from_bytes(b'\x01\x02\x03\x04', 'big')
         expected = 0x01020304
         self.assertEqual(result, expected)
 
-    def testStrangeByteOrder(self):
+    def test_invalid_byte_order(self):
         with self.assertRaises(ValueError):
             from_bytes(b'\x01\x02\x03\x04', 'banana')
 
 
 class TestCounter(unittest.TestCase):
-    def testLittleEndian(self):
+    """
+    Test the actual counter class
+    """
+
+    def test_little_endian(self):
         counter = Counter(b'\x00\x00\x00\x00\x00\x00\x00\x00', 'little')
         self.assertEqual(counter(), b'\x00\x00\x00\x00\x00\x00\x00\x00')
         self.assertEqual(counter(), b'\x01\x00\x00\x00\x00\x00\x00\x00')
 
-    def testBigEndian(self):
+    def test_big_endian(self):
         counter = Counter(b'\x00\x00\x00\x00\x00\x00\x00\x00', 'big')
         self.assertEqual(counter(), b'\x00\x00\x00\x00\x00\x00\x00\x00')
         self.assertEqual(counter(), b'\x00\x00\x00\x00\x00\x00\x00\x01')
 
-    def testMaxValue(self):
+    def test_overflow(self):
         counter = Counter(b'\xff\xff\xff\xff\xff\xff\xff\xff', 'little')
         self.assertEqual(counter(), b'\xff\xff\xff\xff\xff\xff\xff\xff')
         self.assertEqual(counter(), b'\x00\x00\x00\x00\x00\x00\x00\x00')
