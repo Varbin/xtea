1f71efc331b0fd1b10410b7ce428d5af8207d8c1
Simon Biewald <simon@fam-biewald.de>
2022-06-24 15:48:57 +0200

    Remove testing the readme.

--- tests/test_doctest.py
+++ tests/test_doctest.py
@@ -13,11 +13,3 @@ def test_init():
 def test_counter():
     if sys.version_info[0] > 2:
         doctest.testmod(xtea.counter, raise_on_error=True)
-
-def test_readme():
-    if sys.version_info[0] > 2:
-        doctest.testfile(
-            os.path.join(
-                os.path.join(os.path.dirname(__file__), ".."), "README.rst"),
-            raise_on_error=True
-        )
